3c2affaafea65dd8328532b5a0ba58a770919749
bhawiyuga <bhawiyuga@ub.ac.id>
2021-10-26 10:32:02 +0700

    Update installcontainernet.sh
    add user to docker group

--- containernet/installcontainernet.sh
+++ containernet/installcontainernet.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 curl -fsSL https://test.docker.com -o test-docker.sh
 sh test-docker.sh
+sudo usermod -aG docker $(whoami)
 sudo apt update
 sudo apt -y install ansible git aptitude
 git clone https://github.com/containernet/containernet.git
