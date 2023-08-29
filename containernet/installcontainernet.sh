8fea39fb13721962bafa59a4b323954bacd1f88b
Adhitya Bhawiyuga <bhawiyuga@ub.ac.id>
2023-08-29 10:25:55 +0700

    Update installcontainernet.sh

--- containernet/installcontainernet.sh
+++ containernet/installcontainernet.sh
@@ -5,11 +5,11 @@ sudo apt -y install ansible git aptitude
 git clone https://github.com/containernet/containernet.git
 cd containernet/ansible
 sudo ansible-playbook -i "localhost," -c local install.yml
-cd ..
-sudo make develop
-echo "Building docker image...."
-cd ~
-git clone https://github.com/bhawiyuga/jarkom.git
-cd ~/jarkom/containernet/
-docker build -f Dockerfile.ubuntu1804 -t containernet:bionic .
+#cd ..
+#sudo make develop
+#echo "Building docker image...."
+#cd ~
+#git clone https://github.com/bhawiyuga/jarkom.git
+#cd ~/jarkom/containernet/
+#docker build -f Dockerfile.ubuntu1804 -t containernet:bionic .
 cd ~
